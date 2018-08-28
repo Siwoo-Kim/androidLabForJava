d9e16747db75073f84955895675ec9916e021bc8
HOMEPC <skim327@myseneca.ca>
2018-08-28 15:07:54 +0900

    first commit

---
+++ app/src/main/java/com/android/siwoo/androidlabforjava/resource/LabOne.java
@@ -0,0 +1,15 @@
+package com.android.siwoo.androidlabforjava.resource;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.android.siwoo.androidlabforjava.R;
+
+public class LabOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab_one2);
+    }
+}
